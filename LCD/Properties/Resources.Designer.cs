9ed9f8d7ab98ea55549cce5f90d7112081049f11
Ovidiu Radoi <oviradoi2003@yahoo.com>
2018-10-14 16:14:40 +0300

    Delete unneeded files

--- LCD/Properties/Resources.Designer.cs
+++ LCD/Properties/Resources.Designer.cs
@@ -173,16 +173,6 @@ namespace LCD.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Image11 {
-            get {
-                object obj = ResourceManager.GetObject("Image11", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Image2 {
             get {
                 object obj = ResourceManager.GetObject("Image2", resourceCulture);
@@ -326,16 +316,6 @@ namespace LCD.Properties {
         internal static System.Drawing.Bitmap top {
             get {
                 object obj = ResourceManager.GetObject("top", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap UN {
-            get {
-                object obj = ResourceManager.GetObject("UN", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
